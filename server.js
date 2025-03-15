078e3265956f6c1708eca0fca357ca31c0adf962
adfar-136 <adfarrasheed136@gmail.com>
2025-03-15 16:59:20 +0530

    done

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 dotenv.config();
 app.use(cors({
-  origin: ['http://localhost:5174','http://localhost:5173'],
+  origin: ['http://localhost:5174','http://localhost:5173','https://lamicons.com','https://www.lamicons.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
